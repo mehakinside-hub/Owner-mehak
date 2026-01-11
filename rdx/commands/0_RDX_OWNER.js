8aa68e6e8ed23b88b47e06eb7a92a2578b4f290f
mehakinside-hub <gulaabinside@gmail.com>
2026-01-11 18:13:03 +0500

    Update ownerPics array with new image URL

--- rdx/commands/0_RDX_OWNER.js
+++ rdx/commands/0_RDX_OWNER.js
@@ -17,7 +17,7 @@ module.exports = {
     const { threadID, messageID } = event;
 
     const ownerPics = [
-      'https://i.ibb.co/Cp13xBsR/b2edd4c03615.jpg',
+      'https://i.ibb.co/mCNr0YzG/176224e8a6c3.jpg',
       '',
       '',
       ''
